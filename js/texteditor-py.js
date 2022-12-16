6ba86f36c0904d3ec8a2af9cce552a842f606ffe
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2022-12-16 18:10:53 +0530

    Add files via upload

--- js/texteditor-py.js
+++ js/texteditor-py.js
@@ -9,35 +9,6 @@ acehtml.setValue(x ,1);
 
 if(x == null){
 acehtml.setValue(`# write python code here
-
-
-# Python 3 program to print terms of binomial
-# series and also calculate sum of series.
-
-# Function to print the series
-def series(A, X, n):
-
-	# Calculating and printing first term
-	term = pow(A, n)
-	print(term, end = " ")
-
-	# Computing and printing remaining terms
-	for i in range(1, n+1):
-
-		# Find current term using previous terms
-		# We increment power of X by 1, decrement
-		# power of A by 1 and compute nCi using
-		# previous term by multiplying previous
-		# term with (n - i + 1)/i
-		term = int(term * X * (n - i + 1)/(i * A))
-
-		print(term, end = " ")
-	
-# Driver Code
-A = 3; X = 4; n = 5
-series(A, X, n)
-
-# This code is contributed by Smitha Dinesh Semwal.
 `,1);
 }
 }
@@ -286,7 +257,6 @@ var x = main_func.getSession().getValue();
 var fileNameInput = prompt("Enter File Name : \nDon't write extensions for ex-: .py");
 
 
-
 if(fileNameInput == null){
 return;				
 }
@@ -294,7 +264,7 @@ else{
 const blob = new Blob([x], {type: main_func_type});
 const fileUrl = URL.createObjectURL(blob);
 const link = document.createElement("a");
-link.download = fileNameInput;
+link.download = fileNameInput + ".py";
 link.href = fileUrl;
 link.click();				
 }
