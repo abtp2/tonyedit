4d4d7a89e6c7f7e4428121687be5fe859c7f30d2
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2023-01-23 00:55:01 +0530

    Add files via upload

--- js/sourcecodeviewer.js
+++ js/sourcecodeviewer.js
@@ -1,12 +1,46 @@
 document.getElementsByTagName("form")[0].onsubmit = function(){
 event.preventDefault();
+var line = window.navigator.onLine;
+
+
+if(line == true){
 var x = document.getElementById("code-input").value;
 document.getElementById("result-div").style.display ="block";
 document.getElementById("result-link").innerText = x;
 
-fetch('https://api.codetabs.com/v1/proxy?quest=' + x).then((response) => response.text()).then((text) => 
-document.getElementById("result-code").value = text); 
+fetch('https://api.codetabs.com/v1/proxy?quest=' + x).then((response) => response.text()).then((text) =>{
+acehtml.setValue(text ,1)
+var beautify = ace.require("ace/ext/beautify");
+beautify.beautify(acehtml.session);
+});
 }
+else{
+alert("You are offline !! \nTo access the sourcecode you need a internet connection.")
+}
+
+
+
+}
+
+
+
+
+
+/*  
+function htmlToEntities(html) {
+  let entities = {
+    '<': '&lt;',
+    '>': '&gt;',
+  };
+  return html.replace(/[<>]/g, function (c) {
+    return entities[c];
+  });
+}
+ */
+
+
+
+
 
 
 
@@ -22,9 +56,16 @@ document.getElementById("copy").style.color ="#000000";
 
 /* copy code */
 document.getElementById("copy").onclick = function(){
-var x = document.getElementById("result-code").value;
+var x = acehtml.getSession().getValue();
+var clipboard = navigator.clipboard;
+
+if(clipboard != undefined){
 navigator.clipboard.writeText(x);
 document.getElementById("copy").innerHTML ="task_alt";
-document.getElementById("copy").style.color ="#46b100";
+document.getElementById("copy").style.color ="#405b66";
+}
+else{
+alert("Your device not support to copy");
+}
 }
 
